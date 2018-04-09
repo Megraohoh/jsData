e05fa8212b6c5e88a08f7612096a0a35f645fb24
Meghan Debity <mdebity@gmail.com>
2018-04-09 13:27:13 -0500

    updated array

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
 // Define the database as an object
-const HomeInventoryDatabase = {}
+const HomeInventoryDatabase = {
+    HomeInventoryDatabase.gifts = []
+    HomeInventoryDatabase.crafts = []
+    HomeInventoryDatabase.antiques = []
+}
+//gifts
+const skillets = {
+    name: "Iron Skillets",
+    location: "Oven Plate Warming Drawer",
+    description: "I love my iron skillets because they were given to me from family members.",
+}
+const woodPieces = {
+    name: "Wood Pieces",
+    location: "Kitchen Display Shelves",
+    description: "My wooden vase and basket are special to me because they were a gift from my grandfather."   
+}
+const postcards = {
+    name: "Postcards",
+    location: "Leather-bound Journal",
+    description: "When I'm travelling I send out postcards to my friends and family. They in turn send them to me when they are travelling."
+}
+const heartPendant = {
+    name: "Mom's Gold Heart Pendant",
+    location: "Jewelry Box",
+    description: "My dad gifted my mom the pendant and necklace years ago. She loves it but doesn't wear it anymore."
+}
 
-// Define the arrays (a.k.a. tables) in the database to store each type of items
-HomeInventoryDatabase.furniture = []
-HomeInventoryDatabase.crafts = []
-HomeInventoryDatabase.electronics = []
+//crafts
+//antiques
+
+  
+
